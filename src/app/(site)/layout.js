eceb630944c6a824ccc422f7cba1476782784fee
Alex Harris <alexharris6@gmail.com>
2024-12-29 21:50:18 -0800

    add layout width

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -5,9 +5,9 @@ export default function SiteLayout({ children }) {
 
 
   return (
-    <>
+    <div className="w-full md:w-5/6 lg:w-4/6 mx-auto">
     <Header />
     {children}
-    </>
+    </div>
   );
 }
