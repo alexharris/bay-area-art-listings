82974f5f5dfe3bc051ec7c978d2061d1160f5923
Alex Harris <alexharris6@gmail.com>
2024-12-30 22:44:59 -0800

    basic janky password protection of upload page

--- src/app/(site)/upload/page.js
+++ src/app/(site)/upload/page.js
@@ -2,14 +2,30 @@ import React from 'react';
 import GetDataFromSheet from '../../components/getDataFromSheet';
 import DeleteAllListings from '../../components/deleteAllListings';
 
+const UploadPage = ({ searchParams }) => {
+    const isAuthorized = searchParams?.password === 'takethewater';
 
-
-const UploadPage = () => {
     return (
         <div className="p-4">
             <h1 className="text-2xl mb-4">Upload</h1>
-            <GetDataFromSheet />
-            <DeleteAllListings />
+            {!isAuthorized ? (
+                <div>
+                    <form method="get">
+                        <input 
+                            type="password" 
+                            name="password" 
+                            placeholder="Enter password" 
+                            className="mb-4 p-2 border"
+                        />
+                        <button type="submit" className="p-2 bg-black text-white">Submit</button>
+                    </form>
+                </div>
+            ) : (
+                <>
+                    <GetDataFromSheet />
+                    <DeleteAllListings />
+                </>
+            )}
         </div>
     );
 };
