10ea21e5ca0aa8f25784487ca007a6a045f6825c
alex harris <alexharris6@gmail.com>
2025-01-14 10:32:12 -0800

    modify upload to account for duplicate locations

--- src/app/components/uploadSheetData.js
+++ src/app/components/uploadSheetData.js
@@ -15,6 +15,7 @@ const client = createClient({
 export default async function uploadSheetData(data) {
 
   let locationData = []
+  let newLocations = []
 
   // get all of the existing locations
   try {
@@ -24,34 +25,54 @@ export default async function uploadSheetData(data) {
     throw error;
   } 
 
+  // go through the new items and get the locations
+
+  for (const item of data) {
+    // check if the location in the item exists in the existing locations
+      newLocations.push(item['Location']);
+  }
+
+  newLocations = [...new Set(newLocations)];
+
+  console.log(newLocations)
+
+  // add the new locations
+  for (const location of newLocations) {
+    // check if it exists
+    const locationExistsAlready = locationData.some(existingLocation => existingLocation.Name === location);
+    // if it does not exist
+    if (!locationExistsAlready) {
+    // create it
+      const locationResponse = await client.create({
+        _type: 'location',
+        Name: location
+      })   
+      console.log('Data uploaded successfully:', locationResponse); 
+    }
+  }
+
+
   // go through the items being uploaded
+  // we know that all locations exist, but we still need the id
+
+  // get location data now that new ones have been added
+  try {
+    locationData = await client.fetch('*[_type == "location"]');
+  } catch (error) {
+    console.error('Data retrieval failed:', error);
+    throw error;
+  } 
+
+
   try {
     for (const item of data) {
       // check if the location in the item exists in the existing locations
 
-      const locationExistsAlready = locationData.some(location => location.Name === item['Location']);
-      let locationId = ''
-
-      if (!locationExistsAlready) {
-        // if it does not, create it
-        console.log(`Location ${item['Location']} does not exist in locationData`);
-        const locationResponse = await client.create({
-          _type: 'location',
-          Name: item['Location']
-        })
-        console.log('Data uploaded successfully:', locationResponse);
-        locationId = locationResponse._id;
-      } else {
-        // if it does, get the id
-        console.log(locationData.find(location => location.Name === item['Location']))
-        locationId = locationData.find(location => location.Name === item['Location'])._id;
-      }
-
+      let locationId = locationData.find(location => location.Name === item['Location'])._id;
     
-
+      // remove the location because we are replacing it with a reference
       delete item['Location'];
      
-      
       const response = await client.create({
         _type: 'listing',
         Location: {
