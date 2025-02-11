dd582602bdf2475831f7e98fe316eb4d3afed30d
alex harris <alexharris6@gmail.com>
2025-02-11 12:33:17 -0800

    rudimentary working version of EventSource messaging from sheet api

--- src/app/(site)/upload/google/page.js
+++ src/app/(site)/upload/google/page.js
@@ -3,30 +3,50 @@
 import { useState } from 'react';
 
 export default function GoogleSheetPage() {
-  const [data, setData] = useState(null);
+  const [messages, setMessages] = useState([]);
+
+
+  // What I think is happening here:
+  // 1. The user clicks the button
+  // 2. The handleClick function is called
+  // 3. An EventSource is created to listen for messages from the server
+  // 4. The server is called with a fetch request
+  // 5. The server sends messages back to the client
+  // 6. The client listens for messages and adds them to the messages state
+  // 7. When the server is finished, it sends a 'finished' message
+  // 8. The client closes the EventSource
 
   const handleClick = async () => {
-    // console.log('click')
-    // const sheetId = '1uQejuXXnuVwrU1vGDwcWoM4HlZh1XHwRCVqLJE4yqOg';
-    // const range = 'Sheet1!A1:E';
-    // const response = await fetch(`/api/googlesheet?sheetId=${sheetId}&range=${range}`);
-    // const fetchedData = await response.json();
-    // setData(fetchedData);
-
+    // Create an EventSource
+    const eventSource = new EventSource('/api/googlesheet');
+    // Listen for messages
+    eventSource.onmessage = function(event) {
+      const data = JSON.parse(event.data);
+      setMessages(prevMessages => [...prevMessages, data.message]);
+      if (data.message === 'Upload finished') {
+        eventSource.close();
+      }
+    };
+    // Call the API
     const response = await fetch('/api/googlesheet', {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
       },
     });    
-    console.log(response)
   };
  
 
   return (
     <div>
       <button onClick={handleClick}>Fetch Google Sheet Data</button> <br />
-
+      {messages.length > 0 && (
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
