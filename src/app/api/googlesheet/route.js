4127bba71ea6471d200d9ef0e928e6472d61f3e3
alex harris <alexharris6@gmail.com>
2025-02-15 10:35:01 -0800

    basic add listing functionality

--- src/app/api/googlesheet/route.js
+++ src/app/api/googlesheet/route.js
@@ -30,7 +30,7 @@ async function getDataFromSheet(sheetName) {
 async function getPlaceDetailsFromGoogle(newLocation) {
   let textQuery = newLocation + ' bay area art gallery';
   const request = {
-    textQuery,
+    textQuery: textQuery
   };
   const googleQuery = await placesClient.searchText(request, {
     otherArgs: {
@@ -44,32 +44,24 @@ async function getPlaceDetailsFromGoogle(newLocation) {
   } else {
     return googleQuery[0].places[0]
   }
-  
 }
 
 // This function gets the existing locations from Sanity
-async function getExistingLocationsFromSanity() {
+async function getExistingLocationsFromSanity(sanityClient) {
   let existingLocations = [];
-  // Create Sanity Client
-  const client = createClient({
-    projectId: 'ride9vgj',
-    dataset: 'development',
-    token: process.env.SANITY_API_WRITE_TOKEN,
-    useCdn: false,
-    apiVersion: 'v2022-03-07'
-  });
+
   // Get all of the locations that already exist
   try {
-    existingLocations = await client.fetch('*[_type == "location"]');
+    existingLocations = await sanityClient.fetch('*[_type == "location"]');
   } catch (error) {
     console.error('Data retrieval failed:', error);
     throw error;
   }  
+  console.log(existingLocations)
   return existingLocations
 }
 
 async function addGooglePlaceToSanity(googlePlace, newLocation, sanityClient, controller) {
-  console.log(googlePlace)
   await sanityClient.create({
     _type: 'location',
     Name: googlePlace.displayName.text,
@@ -88,6 +80,56 @@ async function addGooglePlaceToSanity(googlePlace, newLocation, sanityClient, co
   return
 }
 
+async function addListingsToSanity(sheetRows, sanityClient, controller) {
+  for (let i = 1; i < sheetRows.length; i++) {
+
+    const row = sheetRows[i];
+    const id = row[0];
+    const highlight = row[1] 
+    const isHighlight = highlight ? true : false;
+    const listingTitle = row[2];
+    let startDate = row[4];
+    let endDate = row[5];
+
+    // Format dates
+    // if the date has a year, leave it alone, otherwise add the current year
+    console.log(startDate, endDate)
+    // Dates have format MMMM DD, YYYY
+    if(startDate.split(',').length === 1) {
+      startDate = startDate + ', ' + new Date().getFullYear();
+    }
+    if(endDate.split(',').length === 1) {
+      endDate = endDate + ', ' + new Date().getFullYear();
+    }
+    // now convert start and end date to to YYYY-MM-DD
+    startDate = new Date(startDate).toISOString().split('T')[0];
+    endDate = new Date(endDate).toISOString().split('T')[0];
+
+
+    // Use the spreadsheet location name to find the corresponding location in Sanity
+    let updatedExistingLocations = await sanityClient.fetch('*[_type == "location"]');
+    let locationId = updatedExistingLocations.find(location => location.OriginalName === row[3])._id;
+
+    await sanityClient.createOrReplace({
+      _type: 'listing',
+      _id: id,
+      Highlight: isHighlight,
+      Event: listingTitle,
+      Location: {
+        _type: 'reference',
+        _ref: locationId,
+        _weak: true
+      },
+      StartDate: startDate,
+      EndDate: endDate,
+    }).then((listingResponse) => {
+      // writeMessageToClient(`Added listing: ${listingTitle}`, controller);
+    }).catch((error) => {
+      // console.error('Error adding listing:', error);
+      // writeMessageToClient(`Unable to add listing: ${listingTitle}. Reason ${error}`, controller);
+    });
+  }
+}
 
 export async function GET(req, res) {
   
@@ -111,7 +153,7 @@ export async function GET(req, res) {
     async start(controller) {
       writeMessageToClient(`Beginning process.`, controller);
       // Get the existing locations from Sanity
-      let existingLocations = await getExistingLocationsFromSanity();
+      let existingLocations = await getExistingLocationsFromSanity(sanityClient);
       // Get the new locations from the Google Sheet
       let newLocations = [];
       let sheetRows = await getDataFromSheet(sheetName);
@@ -131,12 +173,18 @@ export async function GET(req, res) {
           if(googlePlace) {
             writeMessageToClient(`Found info about ${newLocation}. Adding to Sanity`, controller);
             await addGooglePlaceToSanity(googlePlace, newLocation, sanityClient, controller);
+          } else {
+            writeMessageToClient(`Couldn't find info about ${newLocation}. Manually Review.`, controller);
           }
 
         } else {
           writeMessageToClient(`Location exists: ${newLocation}`, controller);
         }
       }
+
+      // With locations done, now we put up the listings
+      writeMessageToClient('--------------------', controller);
+      await addListingsToSanity(sheetRows, sanityClient, controller);
 
       writeMessageToClient('Upload finished', controller);
       controller.close();
