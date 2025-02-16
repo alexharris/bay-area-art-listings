d4d9060765760befb2504772744d90cadc879821
alex harris <alexharris6@gmail.com>
2025-02-15 20:09:58 -0800

    try removing thing from other route

--- src/app/api/googlesheet/route.js
+++ src/app/api/googlesheet/route.js
@@ -1,3 +1,4 @@
+export const maxDuration = 20; // This function can run for a maximum of 20 seconds
 import { google } from 'googleapis';
 import {createClient} from '@sanity/client'
 const { PlacesClient } = require('@googlemaps/places').v1;
