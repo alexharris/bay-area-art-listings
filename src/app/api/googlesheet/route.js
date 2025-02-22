b9cb8f945d8a58bcddb0c6773bfa32b4de0f925d
alex harris <alexharris6@gmail.com>
2025-02-22 11:50:42 -0800

    change console error to log

--- src/app/api/googlesheet/route.js
+++ src/app/api/googlesheet/route.js
@@ -118,7 +118,7 @@ async function addListingsToSanity(sheetRows, sanityClient, controller) {
       StartDate: startDate || '',
       EndDate: endDate || '',
     }).then((listingResponse) => {
-      console.error(listingResponse);
+      console.log(listingResponse);
       writeMessageToClient(`Added listing: ${listingTitle}`, controller);
     }).catch((error) => {
       console.error('Error adding listing:', error);
