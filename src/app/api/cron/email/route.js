03a7d157dc041539914a945e22a59ca7360ec2bd
alex harris <alexharris6@gmail.com>
2025-03-17 11:24:15 -0700

    test cron again

--- src/app/api/cron/email/route.js
+++ src/app/api/cron/email/route.js
@@ -1,9 +1,6 @@
-import { createClient as createSanityClient } from '@sanity/client';
 
 export async function GET() {
   console.log('hello')
-
-
 
 
   return new Response(JSON.stringify({ message: 'Hello World' }), {
