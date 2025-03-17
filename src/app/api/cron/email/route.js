80c9a2b20890c2b1e934b7f153a5aad2749fbaa3
alex harris <alexharris6@gmail.com>
2025-03-17 11:20:13 -0700

    test cron again

--- src/app/api/cron/email/route.js
+++ src/app/api/cron/email/route.js
@@ -1,32 +1,12 @@
-export const dynamic = "force-dynamic";
-
-import postmark from 'postmark';
-
-const serverToken = process.env.POSTMARK_SERVER_TOKEN;
-
-if (!serverToken) {
-  throw new Error('POSTMARK_SERVER_TOKEN environment variable is not set');
-}
-
-const client = new postmark.ServerClient(serverToken);
+import { createClient as createSanityClient } from '@sanity/client';
 
 export async function GET() {
-  console.log('hello');
+  console.log('hello')
 
-  try {
-    const response = await client.sendEmail({
-      "From": "hello@alexharris.online",
-      "To": "hello@alexharris.online",
-      "Subject": "Hello World from Vercel Serverless",
-      "TextBody": 'HELLO WORLD',
-      "MessageStream": "broadcast"
-    });
-    console.log(`Email sent to hello@alexharris.online:`, response);
 
-    return new Response(JSON.stringify({ message: 'Emails sent successfully' }), { status: 200 });
-  } catch (error) {
-    console.error('Error sending emails:', error);
-    return new Response(JSON.stringify({ message: 'Error sending emails', error: error.message }), { status: 500 });
-  }
 
+
+  return new Response(JSON.stringify({ message: 'Hello World' }), {
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
