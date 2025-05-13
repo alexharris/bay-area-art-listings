4f61066966e7b9c3f858667d0585ff33cd986697
alex harris <alexharris6@gmail.com>
2025-05-13 12:47:12 -0700

    reduce google api max duration

--- src/app/api/googlesheet/route.js
+++ src/app/api/googlesheet/route.js
@@ -1,4 +1,4 @@
-export const maxDuration = 600; // This function can run for a maximum of 20 seconds
+export const maxDuration = 60; // This function can run for a maximum of 20 seconds
 
 import { google } from 'googleapis';
 import {createClient} from '@sanity/client'
