3abe43b0f91dabb84209e51f0d270e3864c5f782
alex harris <alexharris6@gmail.com>
2025-06-26 20:47:29 -0700

    try again

--- src/utils/metadata.js
+++ src/utils/metadata.js
@@ -1,6 +1,6 @@
-export const metadata = {
-  title: 'Listing',
-  openGraph: {
-    title: 'Blog',
-  },
-}
+// export const metadata = {
+//   title: 'Listing',
+//   openGraph: {
+//     title: 'Blog',
+//   },
+// }
